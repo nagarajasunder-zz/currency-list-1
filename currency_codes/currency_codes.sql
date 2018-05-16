459606c07fb340077e6fd0a8ac37a18d5f440e20
Raman Singh <ramansaluja849@gmail.com>
2018-05-16 20:11:23 +0530

    Update currency_codes.sql

--- currency_codes/currency_codes.sql
+++ currency_codes/currency_codes.sql
@@ -1,29 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 08, 2018 at 01:49 PM
--- Server version: 10.1.25-MariaDB
--- PHP Version: 5.6.31
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `myprojects_currencyconvertor`
---
-
--- --------------------------------------------------------
-
 --
 -- Table structure for table `currency_codes`
 --
@@ -228,6 +202,3 @@ ALTER TABLE `currency_codes`
 ALTER TABLE `currency_codes`
   MODIFY `currency_code_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=170;COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
